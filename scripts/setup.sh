81df19816393f99dfb3b7131906e135ceb790f89
Leonardo Cardoso <58035475+Leo200467@users.noreply.github.com>
2022-03-27 20:33:34 -0300

    Updated on terraform main, steps and git ignore

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get update  
 sudo apt-get upgrade -y 
 sudo apt install -y docker.io 
+sudo apt install -y pyspark 
 sudo apt install -y docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
 docker --version 
@@ -19,10 +20,11 @@ sudo mv Dockerfile /home/azureuser/airflow
 sudo mv docker-compose.yml /home/azureuser/airflow
 sudo mv airflow.env /home/azureuser/airflow
 sudo mv entrypoint.sh /home/azureuser/airflow/scripts
+sudo mv upload_files_dag.py /home/azureuser/airflow/dags
+sudo mv requirements.txt /home/azureuser/airflow
 cd /home/azureuser/airflow
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/logs
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/dags
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/scripts
-sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/plugin
-sudo docker-compose build 
-sudo docker-compose up 
+sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/plugins
+
