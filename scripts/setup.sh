944052323ad2f6a91095c9777a84c218659915f8
Leonardo Cardoso <58035475+Leo200467@users.noreply.github.com>
2022-04-13 00:43:06 -0300

    Add commands to create better folder structure

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,15 +15,24 @@ sudo mkdir dags
 sudo mkdir logs 
 sudo mkdir scripts 
 sudo mkdir plugins
+sudo mkdir source_data
+sudo mkdir processed_files
 cd /mnt/sharedstorage
 sudo mv Dockerfile /home/azureuser/airflow
 sudo mv docker-compose.yml /home/azureuser/airflow
 sudo mv airflow.env /home/azureuser/airflow
 sudo mv entrypoint.sh /home/azureuser/airflow/scripts
 sudo mv upload_files_dag.py /home/azureuser/airflow/dags
+sudo mv transform_data_dag.py /home/azureuser/airflow/dags
+sudo mv Red.csv /home/azureuser/airflow/source_data
+sudo mv Rose.csv /home/azureuser/airflow/source_data
+sudo mv Sparkling.csv /home/azureuser/airflow/source_data
+sudo mv White.csv /home/azureuser/airflow/source_data
 cd /home/azureuser/airflow
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/logs
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/dags
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/scripts
 sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/plugins
+sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/source_data
+sudo chmod u=rwx,g=rwx,o=rwx /home/azureuser/airflow/processed_files
 
